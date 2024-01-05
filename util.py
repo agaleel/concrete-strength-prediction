d44004312231560474e54eb0da4d4e05370431bb
Ahmad Abdelgaleel <ahmad.abdelgaleel@hotmai.com>
2024-01-06 00:16:38 +0200

    correct model overlapping fit every iteration by initiate a new fresh inistance every iteration

--- util.py
+++ util.py
@@ -15,10 +15,11 @@ def plot_X_y(X,y,cols):
         plt.show()
 
 def runModel(model,X_train,X_test,y_train,y_test):
+        model = type(model)()
         model.fit(X_train,y_train)
         y_pred=model.predict(X_test)
         r2 = r2_score(y_test,y_pred)
-        return r2,y_pred
+        return r2,model
 
 class EvaluateRegression:
     def __init__(self, X_train,X_test,y_train,y_test):
@@ -33,30 +34,30 @@ class EvaluateRegression:
         if X_test is None:
             X_test=self.X_test
 
-        lm = LinearRegression()
-        return runModel(lm,X_train,X_test,self.y_train,self.y_test)
+        return runModel(LinearRegression(),X_train,X_test,self.y_train,self.y_test)
     
     def polymomial(self,model=None):
 
-        best = dict(zip(['degree','score','y_pred'],[0,0,None]))
+        best = dict(zip(['degree','score','model'],[0,0,None]))
         degrees = range(2,config['max_plynomial_degree']+1) 
         for d in degrees:
             poly = PolynomialFeatures(degree=d)
             Xp_train = poly.fit_transform(self.X_train)
             Xp_test = poly.transform(self.X_test)
             if model is None:
-                r2, y_pred = self.simpleLinear(Xp_train,Xp_test)
-            else:
-                r2, y_pred = runModel(model,Xp_train,Xp_test,self.y_train,self.y_test)
+                model = LinearRegression()
+                
+            r2, model = runModel(model,Xp_train,Xp_test,self.y_train,self.y_test)
 
             if r2 > best['score']:
                 best['degree'] = d
                 best['score'] = r2
-                best['y_pred'] = y_pred
+                best['model'] = model
+            
         return best
     
     def evaluateRidge(self):
-        best = dict(zip(['alpha','degree','score','y_pred'],[0,0,0,None]))
+        best = dict(zip(['alpha','degree','score','model'],[0,0,0,None]))
         alphas = config['alpha_range']
         for a in alphas:
             model = Ridge(alpha=a)
@@ -65,12 +66,12 @@ class EvaluateRegression:
                 best['alpha'] = a
                 best['degree'] = result['degree']
                 best['score'] = result['score']
-                best['y_pred'] = result['y_pred']
+                best['model'] = result['model']
 
         return best
     
     def evaluateLasso(self):
-        best = dict(zip(['alpha','degree','score','y_pred'],[0,0,0,None]))
+        best = dict(zip(['alpha','degree','score','model'],[0,0,0,None]))
         alphas = config['alpha_range']
         for a in alphas:
             model = Lasso(alpha=a)
@@ -79,41 +80,38 @@ class EvaluateRegression:
                 best['alpha'] = a
                 best['degree'] = result['degree']
                 best['score'] = result['score']
-                best['y_pred'] = result['y_pred']
+                best['model'] = result['model']
 
         return best
     
     def selectBestModel(self):
-        best = dict(zip(['model_name','alpha','degree','score','y_pred'],['Simple Linear Regression',0,0,0,None]))
+        best = dict(zip(['alpha','degree','score','model'],[0,0,0,None]))
         
         # evaluate Simple Linear Regression Model
-        r2,y_pred = self.simpleLinear()
+        r2,model = self.simpleLinear()
         best['score'] = r2
-        best['y_pred'] = y_pred
+        best['model'] = model
 
         # evaluate  Polynomial Regession without Regularization
         results = self.polymomial()
         if results['score'] > best['score']:
-            best['model_name'] = 'Polynomial Regession without Regularization'
             best['score'] = results['score']
             best['degree'] = results['degree']
-            best['y_pred'] = results['y_pred']
+            best['model'] = results['model']
 
         # evaluate Ridge
         results = self.evaluateRidge()
         if results['score'] > best['score']:
-            best['model_name'] = 'Ridge'
             best['score'] = results['score']
             best['degree'] = results['degree']
-            best['y_pred'] = results['y_pred']
+            best['model'] = results['model']
 
 
         # evaluate Lasso
         results = self.evaluateLasso()
         if results['score'] > best['score']:
-            best['model_name'] = 'Lasso'
             best['score'] = results['score']
             best['degree'] = results['degree']
-            best['y_pred'] = results['y_pred']
+            best['model'] = results['model']
 
         return best
